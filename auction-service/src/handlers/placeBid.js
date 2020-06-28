3b2e8ae4594b0d54ea676f5d3e4103a4960358f0
Guilherme de Lemos <guilherme.eti@gmail.com>
2020-06-27 22:27:05 -0300

    Identification

--- auction-service/src/handlers/placeBid.js
+++ auction-service/src/handlers/placeBid.js
@@ -10,11 +10,23 @@ const dynamodb = new AWS.DynamoDB.DocumentClient();
 async function placeBid(event, context) {
     const { id } = event.pathParameters;
     const { amount } = event.body;
+    const { email } = event.requestContext.authorizer;
 
     const auction = await getAuctionById(id);
 
+    // Auction status validation
     if(auction.status !== 'OPEN') {
         throw new createError.Forbidden('You cannot bid on closed auctions!');
+    }
+
+    // Bid identity validation
+    if(auction.seller === email) {
+        throw new createError.Forbidden('You cannot bid on your own auctions!');
+    }
+
+    // Avoid double bidding
+    if(auction.highestBid.bidder === email) {
+        throw new createError.Forbidden('You are already the highest bidder!');
     }
 
     if(amount <= 0) {
@@ -28,9 +40,10 @@ async function placeBid(event, context) {
     const params = {
         TableName: process.env.AUCTIONS_TABLE_NAME,
         Key: { id },
-        UpdateExpression: 'set highestBid.amount = :amount',
+        UpdateExpression: 'set highestBid.amount = :amount, highestBid.bidder = :bidder',
         ExpressionAttributeValues: {
             ':amount': amount,
+            ':bidder': email,
         },
         ReturnValues: 'ALL_NEW',
     };
