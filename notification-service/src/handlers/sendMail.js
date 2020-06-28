815a0270eb79093749afadcc56f1d7977b5a1b6b
Guilherme de Lemos <guilherme.eti@gmail.com>
2020-06-27 23:55:33 -0300

    SendMail Queue + Lambda Trigger

--- notification-service/src/handlers/sendMail.js
+++ notification-service/src/handlers/sendMail.js
@@ -3,30 +3,41 @@ import AWS from 'aws-sdk';
 const ses = new AWS.SES({region: 'us-east-1'});
 
 async function sendMail(event, context) {
-  const params = {
-    Source: 'guilherme.eti@gmail.com',
-    Destination: {
-      ToAddresses: ['guilherme.eti+1@gmail.com'],
-    },
-    Message: {
-      Body: {
-        Text: {
-          Data: 'Hello',
+  const records = event.Records;
+
+  let result = [];
+  for(let i = 0; i < records.length; i++) {
+    console.log('record processing', records[i]);
+
+    const email = JSON.parse(records[i].body);
+    const { subject, body, recipient } = email;
+
+    const params = {
+      Source: 'guilherme.eti@gmail.com',
+      Destination: {
+        ToAddresses: [recipient],
+      },
+      Message: {
+        Body: {
+          Text: {
+            Data: body,
+          },
+        },
+        Subject: {
+          Data: subject,
         },
       },
-      Subject: {
-        Data: 'Test Mail',
-      },
-    },
-  };
+    };
 
-  try {
-    const result = await ses.sendEmail(params).promise();
-    console.log(result);
-    return result;
-  } catch(error) {
-    console.error(error);
+    try {
+      const mailResult = await ses.sendEmail(params).promise();
+      result.push(mailResult);
+    } catch(error) {
+      console.error(error);
+    }
   }
+  console.log('sendMail finished', result);
+  return result;
 }
 
 export const handler = sendMail;
